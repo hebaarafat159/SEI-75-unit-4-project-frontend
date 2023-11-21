a8fd306386b79163565214a3876ced94e2df57c4
hebaarafat159 <heba.arafat159@gmail.com>
2023-11-21 23:23:51 +0000

    delete book

--- src/components/buttonsComponent.js
+++ src/components/buttonsComponent.js
@@ -1,19 +1,30 @@
 import { Box, Stack, Button, Link } from '@mui/joy';
+import React, { Component } from 'react';
+import axios from "axios";
+import utils from '../utilities/util.js'
 
-export default class ButtonsComponent extends React.Component {
-    render() {
-        return (
-            <div>Buttons component</div>
-            // <Stack gap={4} sx={{ mt: 2 }}>
-            //     <Stack gap={4} sx={{ mt: 2 }}>
-            //         <Button fullWidth>
-            //             Edit {book.title}
-            //         </Button>
-            //         <Button fullWidth>
-            //             Delete
-            //         </Button>
-            //     </Stack>
-            // </Stack>
-        )
+export default function ButtonsComponent({ book, removeBook }) {
+
+    async function deleteBook(book) {
+        const { data } = await axios.delete(`${process.env.REACT_APP_URL_APP_PATH}/books/${book.id}/delete/`, book,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${utils.getUserToken()}`,
+                },
+            },
+            {
+                withCredentials: true
+            },
+        );
+        removeBook(book)
+        window.location.href = "/";
     }
+
+    return <Stack gap={4} sx={{ mt: 2 }}>
+        <Stack gap={4} sx={{ mt: 2 }}>
+            <Button fullWidth> Edit {book.title} </Button>
+            <Button onClick={()=> deleteBook(book)} fullWidth> Delete {book.id}</Button>
+        </Stack>
+    </Stack>
 }
--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import utils from '../utilities/util.js'
-
+import ButtonsComponent from './buttonsComponent.js'
 export default function Home() {
   const [userToken, setUserToken] = useState(null)
   const [books, setBooks] = useState([])
+  const [change, setChange] = useState(null)
 
+  function removeBook(book) {
+    if (book)
+      setChange(book)
+  }
   const fetchData = async (token) => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_URL_APP_PATH}/books/`, {
@@ -25,20 +30,17 @@ export default function Home() {
     if (utils.getUserToken() === null)
       window.location.href = "/login";
     else {
-      // Load books data
-      //console.log(`token :: ${utils.getUserToken()}`)
       fetchData()
     }
-  }, [userToken]);
+  }, [userToken, change]);
 
   return (
-
     <div className="Auth-form-container">
       <h1> Home Page </h1>
       {(books) ? books.map((book) => (
         <div>
           <h4> ID : {book.id} , Title: {book.title}</h4>
-          <buttonsComponent/>
+          <ButtonsComponent book={book} removeBook={removeBook} />
         </div>
       )) : null}
     </div>
